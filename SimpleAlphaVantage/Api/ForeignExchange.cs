96ed68efdf607b9e9ae64b1d9d8643fd36a419c5
Greg Walker <rejk0rz+gh@gmail.com>
2018-07-06 18:32:55 -0400

    The single 4X endpoint added

---
+++ SimpleAlphaVantage/Api/ForeignExchange.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SimpleAlphaVantage.ResponseModels;
+using SimpleAlphaVantage.Utilities;
+
+namespace SimpleAlphaVantage.Api
+{
+    public class ForeignExchange
+    {
+        private readonly string _apiKey;
+        private readonly GenericApiClient _apiClient;
+
+        private readonly DataFormat _dataFormat = DataFormat.json;
+
+        public ForeignExchange(string apiKey, GenericApiClient apiClient = null)
+        {
+            _apiKey = apiKey;
+            _apiClient = apiClient ?? new GenericApiClient(ConfigureClient);
+        }
+
+        protected void ConfigureClient(HttpClient client)
+        {
+            //client.DefaultRequestHeaders
+            //    .Accept
+            //    .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        protected Uri BuildUri(ApiFunction function)
+        {
+            return new Uri($"https://www.alphavantage.co/query?function={function}&apikey={_apiKey}&datatype={_dataFormat}");
+        }
+
+
+        //TODO is the above a good candidate for an abstract class members?
+
+
+
+
+
+        public async Task<CurrencyExchangeRate> CurrencyExchangeRate(string fromCurrency, string toCurrency)
+        {
+            var function = ApiFunction.CURRENCY_EXCHANGE_RATE;
+
+            return await _apiClient.SendGetAsync<CurrencyExchangeRate>(BuildUri(function), new Dictionary<string, string>
+            {
+                {"from_currency", fromCurrency},
+                {"to_currency", toCurrency}
+            });
+        }
+    }
+}
---
+++ SimpleAlphaVantage/ResponseModels/CurrencyExchangeRate.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace SimpleAlphaVantage.ResponseModels
+{
+    public class CurrencyExchangeRate
+    {
+        [JsonProperty("Realtime Currency Exchange Rate")]
+        public ExchangeRateData Data { get; set; }
+    }
+
+    public class ExchangeRateData
+    {
+        [JsonProperty("1. From_Currency Code")]
+        public string FromCurrencyCode { get; set; }
+        [JsonProperty("2. From_Currency Name")]
+        public string FromCurrencyName { get; set; }
+        [JsonProperty("3. To_Currency Code")]
+        public string ToCurrencyCode { get; set; }
+        [JsonProperty("4. To_Currency Name")]
+        public string ToCurrencyName { get; set; }
+        [JsonProperty("5. Exchange Rate")]
+        public string ExchangeRate { get; set; }
+        [JsonProperty("6. Last Refreshed")]
+        public string LastRefreshed { get; set; }
+        [JsonProperty("7. Time Zone")]
+        public string TimeZone { get; set; }
+    }
+}
