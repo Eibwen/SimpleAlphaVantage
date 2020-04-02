7696469f604a24e33b7d246e064c176b6acc3763
Greg Walker <rejk0rz+gh@gmail.com>
2020-04-02 10:10:45 -0400

    Better error handling

---
+++ SimpleAlphaVantage/Exceptions/DeserializationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleAlphaVantage.Exceptions
+{
+    public class DeserializationException : AlphaVantageException
+    {
+        public string RawResponse { get; }
+
+        public DeserializationException(string message, string rawResponse) : base(message)
+        {
+            RawResponse = rawResponse;
+        }
+
+        public DeserializationException(string message, string rawResponse, Exception innerException) : base(message, innerException)
+        {
+            RawResponse = rawResponse;
+        }
+    }
+}
---
+++ SimpleAlphaVantage/ResponseModels/ErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace SimpleAlphaVantage.ResponseModels
+{
+    public class ErrorResponse
+    {
+        [JsonProperty("Error Message")]
+        public string ErrorMessage { get; set; }
+    }
+}
